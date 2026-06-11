de00e9796bc1d29c7aa6932aa8a510a29051f55c
lin onetwo <linonetwo012@gmail.com>
2026-06-12 01:04:47 +0800

    refactor: 清理 constants.ts 未用常量；删 background-actions TID 冗余元数据；移动 auto-language.tid 到 wiki；补全 startup meta 文件

--- src/CPLPlugin/startup/constants.ts
+++ src/CPLPlugin/startup/constants.ts
@@ -1,35 +1,9 @@
-export const CPL_API_BASE = '/cpl';
-export const API_STATUS_TIDDLER = '$:/temp/CPL-Repo/api-status';
-export const API_TYPE_TIDDLER = '$:/temp/CPL-Repo/server-type';
-export const API_MESSAGE_TIDDLER = '$:/temp/CPL-Repo/server-message';
-export const REPO_TYPE_TIDDLER = '$:/temp/CPL-Repo/mirror-type';
 export const LEGACY_MIRROR_CONFIG_TITLE =
   '$:/plugins/Gk0Wk/CPL-Repo/config/current-repo';
 export const STATIC_REPO_CONFIG_TITLE =
   '$:/plugins/Gk0Wk/CPL-Repo/config/current-static-repo';
-export const MIRROR_CONFIG_TITLE = STATIC_REPO_CONFIG_TITLE;
-export const MIRROR_STATIC_REPOS_TITLE =
-  '$:/plugins/Gk0Wk/CPL-Repo/config/static-repos';
-export const SERVER_REPO_CONFIG_TITLE =
-  '$:/plugins/Gk0Wk/CPL-Repo/config/current-server-repo';
-export const SERVER_CONFIG_TITLE = SERVER_REPO_CONFIG_TITLE;
-export const LEGACY_SERVER_LIST_TITLE =
-  '$:/plugins/Gk0Wk/CPL-Repo/config/servers';
-export const SERVER_LIST_TITLE =
-  '$:/plugins/Gk0Wk/CPL-Repo/config/server-repos';
-export const ALL_PLUGIN_STATS_REFRESH_TITLE =
-  '$:/temp/CPL-Server/all-plugin-stats-refresh-token';
-export const PLUGIN_ACTIVITY_REFRESH_TITLE =
-  '$:/temp/CPL-Server/plugin-activity-refresh-token';
-export const COMMENTS_CENTER_REFRESH_TITLE =
-  '$:/temp/CPL-Server/comments-center-refresh-token';
 export const SERVER_PROBE_REFRESH_TITLE =
   '$:/temp/CPL-Server/probe-refresh-token';
-export const AUTH_REFRESH_TITLE = '$:/temp/CPL-Server/auth-refresh-token';
-export const AUTH_CONFIG_RESPONSE_TITLE =
-  '$:/temp/CPL-Server/auth-config-response';
-export const AUTH_STATUS_RESPONSE_TITLE =
-  '$:/temp/CPL-Server/auth-status-response';
 export const BUILD_STATUS_REFRESH_TITLE =
   '$:/temp/CPL-Server/build-status-refresh-token';
 export const BUILD_STATUS_RESPONSE_TITLE =
